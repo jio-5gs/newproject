2ce2d6b2f68d78d27dfb167abaab9182373c6cd3
KandlaguntaVenkataSivaNiranjanReddy <kandlaguntaniranjanreddy1231@gmail.com>
2024-08-11 23:45:56 +0530

    java doube

--- five.java
+++ five.java
@@ -2,4 +2,10 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World");
   }
+} 
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
 }
+
